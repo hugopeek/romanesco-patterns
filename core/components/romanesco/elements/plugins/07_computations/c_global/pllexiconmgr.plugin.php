c6f853eff419596d99b34078efd1973f69f6b3c6
Hugo Peek <hugo@fractal-farming.com>
2017-12-16 20:18:46 +0800

    Load custom CSS styles for CB Repeaters

--- core/components/romanesco/elements/plugins/07_computations/c_global/pllexiconmgr.plugin.php
+++ core/components/romanesco/elements/plugins/07_computations/c_global/pllexiconmgr.plugin.php
@@ -1,2 +1,6 @@
 <?php
 $modx->controller->addLexiconTopic('romanescobackyard:manager');
+
+# Load custom CSS styles for ContentBlocks
+$url = $modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css';
+$modx->regClientCss($url);
