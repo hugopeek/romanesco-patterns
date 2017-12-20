b0b71578ae5a18761caf1da5ec5005c9ade0d593
Hugo Peek <hugo@fractal-farming.com>
2017-12-20 19:16:29 +0800

    Enable editor to add custom (pre)hooks to FormBlocks forms

---
+++ core/components/romanesco/elements/snippets/06_formulas/f_formblocks/f_fb_hooks/fbemptyhook.snippet.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * 
+ * fbEmptyHook
+ *
+ * Dummy hook to trick FormIt into not throwing errors saying there are no hooks,
+ * and to make the comma separation between hooks a little easier.
+ */
+
+// Nothing to see here, move along..
+return true;
