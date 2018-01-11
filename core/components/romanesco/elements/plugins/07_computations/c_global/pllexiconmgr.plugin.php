b13d6e254dacf8d80a13ca2c99a9a298428623a1
Hugo Peek <hugo@fractal-farming.com>
2018-01-11 23:51:18 +0800

    Mute strange and elusive ImagePlus output line

--- core/components/romanesco/elements/plugins/07_computations/c_global/pllexiconmgr.plugin.php
+++ core/components/romanesco/elements/plugins/07_computations/c_global/pllexiconmgr.plugin.php
@@ -1,6 +1,11 @@
 <?php
 $modx->controller->addLexiconTopic('romanescobackyard:manager');
 
-# Load custom CSS styles for ContentBlocks
-$url = $modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css';
-$modx->regClientCss($url);
+// Load custom CSS styles for ContentBlocks
+$modx->regClientCss($modx->getOption('base_url') . 'assets/components/romanescobackyard/css/contentblocks.css');
+
+// Mute strange and elusive ImagePlus output line
+if ($modx->event->name == 'OnManagerPageAfterRender') {
+    $outputToRemove = $modx->getOption('core_path') . 'components/imageplus/elements/tv/output/';
+    $controller->content = str_replace($outputToRemove, '', $modx->controller->content);
+}
