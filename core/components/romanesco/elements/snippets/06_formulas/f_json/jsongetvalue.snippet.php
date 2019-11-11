6b950ef09207ac37db2ce3dfa6a625fd68c5a226
Hugo Peek <hugo@fractal-farming.com>
2019-11-11 20:51:40 +0800

    Add ability to search for multiple instances of key in jsonGetValue

--- core/components/romanesco/elements/snippets/06_formulas/f_json/jsongetvalue.snippet.php
+++ core/components/romanesco/elements/snippets/06_formulas/f_json/jsongetvalue.snippet.php
@@ -1,17 +1,44 @@
 <?php
-$input = $modx->getOption('json', $scriptProperties);
-$key = $modx->getOption('key', $scriptProperties);
+$input = $modx->getOption('json', $scriptProperties, $input);
+$key = $modx->getOption('key', $scriptProperties, $options);
 $tpl = $modx->getOption('tpl', $scriptProperties, '');
+$outputSeparator = $modx->getOption('outputSeparator', $scriptProperties, ',');
+$toPlaceholder = $modx->getOption('toPlaceholder', $scriptProperties, false);
 
+// @todo: test if input is valid JSON, otherwise NULL is returned
 $input = utf8_encode($input);
 $array = json_decode($input, true);
 
-$output = $array[$key];
+// Single result from flat array
+if ($array[$key]) {
+    $output = $array[$key];
 
-if ($tpl) {
-    $output = $modx->getChunk($tpl, array(
-        'content' => $output
-    ));
+    if ($tpl) {
+        $output = $modx->getChunk($tpl, array(
+            'content' => $output
+        ));
+    }
+};
+
+// Multiple keys from multidimensional array
+if (is_array($array[0])) {
+    foreach ($array as $item) {
+        $output[] = $item[0][$key];
+
+        if ($tpl) {
+            $output[] = $modx->getChunk($tpl, array(
+                'content' => $output
+            ));
+        }
+    }
+
+    $output = implode(',',$output);
+}
+
+// Output either to placeholder, or directly
+if ($toPlaceholder) {
+    $modx->setPlaceholder($toPlaceholder, $output);
+    return '';
 }
 
 return $output;
