55bbffc60a2e598e0d031ccae666f52e1543ca69
Hugo Peek <hugo@fractal-farming.com>
2020-01-09 12:05:19 +0800

    Add round and removeDuplicateLines snippets

---
+++ core/components/romanesco/elements/snippets/06_formulas/f_modifiers/removeduplicatelines.snippet.php
@@ -0,0 +1,22 @@
+<?php
+$lines = $modx->getOption('input', $scriptProperties, $input);
+$file = $modx->getOption('file', $scriptProperties, '');
+
+// Check first if we're dealing with an external file
+if ($file) {
+    $lines = file_get_contents($file);
+}
+
+// Create an array of all lines inside the input
+$lines = explode("\n", strtolower($lines));
+$i = 0;
+
+// Check if the lines array contains duplicates
+$output = array_unique($lines);
+$output = array_filter($output);
+
+if (is_array($output)) {
+    return implode("\n", $output);
+} else {
+    return $output;
+}
---
+++ core/components/romanesco/elements/snippets/06_formulas/f_modifiers/round.snippet.php
@@ -0,0 +1,3 @@
+<?php
+if ($input == '') return '';
+return round($input,$options);
