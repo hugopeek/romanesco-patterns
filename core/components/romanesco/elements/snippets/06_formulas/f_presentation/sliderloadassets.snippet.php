cfe612ba2ed8a0b2547fb1d36742760dba1451c4
Hugo Peek <hugo@fractal-farming.com>
2020-01-10 17:39:09 +0800

    Load minified and concatenated versions of slider assets

--- core/components/romanesco/elements/snippets/06_formulas/f_presentation/sliderloadassets.snippet.php
+++ core/components/romanesco/elements/snippets/06_formulas/f_presentation/sliderloadassets.snippet.php
@@ -10,14 +10,15 @@ $assetsPathJS = $modx->getOption('romanesco.custom_js_path', $scriptProperties, 
 $assetsPathVendor = $modx->getOption('romanesco.custom_vendor_path', $scriptProperties, '');
 
 // Head
-$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick.css');
-$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick-theme.css');
-$modx->regClientCSS($assetsPathVendor . '/slick-lightbox/slick-lightbox.css');
+//$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick.css');
+//$modx->regClientCSS($assetsPathVendor . '/slick-carousel/slick-theme.css');
+//$modx->regClientCSS($assetsPathVendor . '/slick-lightbox/slick-lightbox.css');
+$modx->regClientCSS($assetsPathCSS . '/slider.min.css');
 
 // Footer
 //$modx->regClientScript('//code.jquery.com/jquery-migrate-1.2.1.min.js');
 $modx->regClientScript($assetsPathVendor . '/slick-carousel/slick.min.js');
 $modx->regClientScript($assetsPathVendor . '/slick-lightbox/slick-lightbox.min.js');
-$modx->regClientScript($assetsPathJS . '/slider.js');
+$modx->regClientScript($assetsPathJS . '/slider.min.js');
 
 return '';
