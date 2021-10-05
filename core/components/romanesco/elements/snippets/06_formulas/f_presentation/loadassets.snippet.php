d03e22502f1017406b3c964e83041d06d72553e3
Hugo Peek <hugo@fractal-farming.com>
2021-10-05 20:16:32 +0800

    Improvements to loadAssets snippet
    - Add option to load custom assets
    - More control over CSS async setting
    - Documentation

--- core/components/romanesco/elements/snippets/06_formulas/f_presentation/loadassets.snippet.php
+++ core/components/romanesco/elements/snippets/06_formulas/f_presentation/loadassets.snippet.php
@@ -2,7 +2,47 @@
 /**
  * loadAssets
  *
- * Generic asset loader. Needs component value to decide which assets to load.
+ * Generic asset loader. Needs a component value to decide which assets to load.
+ *
+ * The MODX regClient functions will automatically filter duplicate statements,
+ * so you don't need to worry about an element being loaded more than once (as
+ * opposed to putting the link / script in your templates).
+ *
+ * External javascript sources are loaded with defer, to keep their loading
+ * sequence intact (based on position in HTML). CSS is loaded asynchronously if
+ * it's not directly affecting page styling (modals for example) and if
+ * critical CSS is enabled.
+ *
+ * Cache busting and minification is also taken care of. The biggest limitation
+ * is that you can only add one component at the time.
+ *
+ * You can add custom CSS or JS by defining a custom component:
+ *
+ * [[loadAssets?
+ *     &component=`custom`
+ *     &css=`[[++romanesco.custom_css_path]]/custom[[+minify]][[+cache_buster_css]].css`
+ *     &js=`[[++romanesco.custom_js_path]]/custom[[+minify]][[+cache_buster_js]].js`
+ *     &inlineJS=`
+ *         <script>
+ *         window.addEventListener('DOMContentLoaded', function() {
+ *             console.log('Something very custom');
+ *         });
+ *         </script>
+ *     `
+ * ]]
+ *
+ * This does support multiple CSS and JS references. Just add them in a valid
+ * JSON array:
+ *
+ * [[loadAssets?
+ *     &component=`custom`
+ *     &css=`[
+ *         "[[++romanesco.custom_css_path]]/custom1[[+minify]][[+cache_buster_css:empty=``]].css",
+ *         "[[++romanesco.custom_css_path]]/custom2[[+minify]][[+cache_buster_css:empty=``]].css"
+ *     ]`
+ * ]]
+ *
+ * You can prevent asynchronous loading of custom CSS with &cssAsync=`0`.
  *
  * @var modX $modx
  * @var array $scriptProperties
@@ -29,6 +69,11 @@ if (!$component) {
     return;
 }
 
+// Custom components bring their own CSS or JS
+$customCSS = $modx->getOption('css', $scriptProperties, '');
+$customJS = $modx->getOption('js', $scriptProperties, '');
+$customInlineJS = $modx->getOption('inlineJS', $scriptProperties, '');
+
 // Load strings to insert in asset paths when cache busting is enabled
 $cacheBusterCSS = $romanesco->getCacheBustingString('CSS');
 $cacheBusterJS = $romanesco->getCacheBustingString('JS');
@@ -39,46 +84,53 @@ if ($modx->getObject('cgSetting', array('key' => 'minify_css_js'))->get('value')
     $minify = '.min';
 }
 
-// Load component asynchronously if critical CSS is enabled
-$async = '';
+// Define conditions for loading CSS asynchronously
+$async = array(
+    'always' => ' media="print" onload="this.media=\'all\'"',
+    'critical' => '',
+    'custom' => '',
+);
 if ($romanesco->getConfigSetting('critical_css', $modx->resource->get('context_key'))) {
-    $async = ' media="print" onload="this.media=\'all\'"';
+    $async['critical'] = $async['always'];
+}
+if ($modx->getOption('cssAsync', $scriptProperties, 1)) {
+    $async['custom'] = $async['always'];
 }
 
 switch ($component) {
     case 'hub':
     case 'status grid':
     case 'status-grid':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/table.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/step.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/modal.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/table.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['critical'] . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/step.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['critical'] . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['always'] . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/modal.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['always'] . '>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterJS . '.js"></script>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathDist . '/components/modal.min' . $cacheBusterJS . '.js"></script>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathVendor . '/tablesort/tablesort.min' . $cacheBusterJS . '.js"></script>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathJS . '/hub' . $minify . $cacheBusterJS . '.js"></script>');
-    break;
+        break;
     case 'table':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/table.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/table.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['critical'] . '>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathVendor . '/tablesort/tablesort.min' . $cacheBusterJS . '.js"></script>');
         break;
     case 'step':
     case 'steps':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/step.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/step.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['critical'] . '>');
         break;
     case 'modal':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/modal.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['always'] . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/modal.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['always'] . '>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterJS . '.js"></script>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathDist . '/components/modal.min' . $cacheBusterJS . '.js"></script>');
         break;
     case 'dimmer':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['always'] . '>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathDist . '/components/dimmer.min' . $cacheBusterJS . '.js"></script>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathJS . '/dimmer'. $minify . $cacheBusterJS . '.js"></script>');
         break;
     case 'syntax-highlighting':
-        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathVendor . '/prism/prism.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async . '>');
+        $modx->regClientStartupHTMLBlock('<link href="' . $assetsPathVendor . '/prism/prism.min' . $cacheBusterCSS . '.css" rel="stylesheet" type="text/css"' . $async['critical'] . '>');
         $modx->regClientHTMLBlock('<script defer src="' . $assetsPathVendor . '/prism/prism.min' . $cacheBusterJS . '.js"></script>');
         break;
     case 'map':
@@ -91,6 +143,34 @@ switch ($component) {
             $modx->getOption('romanesco.leaflet_js_url', $scriptProperties, '') .
             '" integrity="' . $modx->getOption('romanesco.leaflet_js_integrity', $scriptProperties, '') .
             '" crossorigin=""></script>');
+    case 'custom':
+        function is_json($string) {
+            json_decode($string);
+            return json_last_error() === JSON_ERROR_NONE;
+        }
+        if ($customCSS) {
+            if (is_json($customCSS)) {
+                $customCSS = json_decode($customCSS, true);
+                foreach ($customCSS as $CSS) {
+                    $modx->regClientStartupHTMLBlock('<link href="' . $CSS . '" rel="stylesheet" type="text/css"' . $async['custom'] . '>');
+                }
+            } else {
+                $modx->regClientStartupHTMLBlock('<link href="' . $customCSS . '" rel="stylesheet" type="text/css"' . $async['custom'] . '>');
+            }
+        }
+        if ($customJS) {
+            if (is_json($customJS)) {
+                $customJS = json_decode($customJS, true);
+                foreach ($customJS as $JS) {
+                    $modx->regClientHTMLBlock('<script defer src="' . $JS . '"></script>');
+                }
+            } else {
+                $modx->regClientHTMLBlock('<script defer src="' . $customJS . '"></script>');
+            }
+        }
+        if ($customInlineJS) {
+            $modx->regClientHTMLBlock($customInlineJS);
+        }
         break;
 }
 
