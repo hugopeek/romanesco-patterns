5e61c0c305beb0e76d1d4e11ed174248a9bfb1cd
Hugo Peek <hugo@fractal-farming.com>
2022-03-09 17:24:36 +0800

    Load FormBlocks forms in specific context
    This works by placing them in a subfolder under the Forms parent container.
    
    Make sure the subfolder alias matches the context key!
    
    Forms that are placed in the parent container are visible in all contexts. This behaviour is the same as in Global Backgrounds.

--- core/components/romanesco/elements/snippets/06_formulas/f_formblocks/fbgetforms.snippet.php
+++ core/components/romanesco/elements/snippets/06_formulas/f_formblocks/fbgetforms.snippet.php
@@ -1,17 +1,44 @@
 <?php
+/**
+ * fbGetForms snippet
+ *
+ * @var modX $modx
+ * @var array $scriptProperties
+ *
+ * @package romanesco
+ */
+
 if (!($modx->resource instanceof modResource)) return;
 
 $context = $modx->getContext($modx->resource->get('context_key'));
+$contextKey = $context->get('key');
+$contextName = $context->get('name');
 $parentID = $context->getOption('formblocks.container_id') ?? $modx->getOption('formblocks.container_id');
 
 $output = $modx->runSnippet('getResources', (array(
     'parents' => $parentID,
     'limit' => 0,
+    'depth' => 2,
+    'showHidden' => 0,
+    'showUnpublished' => 1,
+    'tpl' => '@INLINE ['.$contextName.'] [[+pagetitle]]=[[+id]]',
+    'sortby' => 'menuindex',
+    'sortdir' => 'ASC',
+    'where' => '[{"template:IN":[10,19]},{"uri:LIKE":"%/'.$contextKey.'/%"}]',
+)));
+if ($output) {
+    $output .= "\n";
+}
+$output .= $modx->runSnippet('getResources', (array(
+    'parents' => $parentID,
+    'limit' => 0,
+    'depth' => 0,
     'showHidden' => 0,
     'showUnpublished' => 1,
     'tpl' => '@INLINE [[+pagetitle]]=[[+id]]',
     'sortby' => 'menuindex',
     'sortdir' => 'ASC',
+    'where' => '{"template:=":"10"}',
 )));
 
 return $output;
